e1ddd29a00d635a584714d882f0b8ab5719767d4
solarconstant <ashishshubh001@gmail.com>
2021-02-15 14:21:31 +0530

    Add Spotipy and OpenWeather

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ A simple whatsapp bot that suggests music (Spotify links) based on user's mood.
 
 HTTP requests are being sent to my localhost which id made public with ngrok.<br />
 Pretty basic stuff right now.<br />
+Added Spotify API.<br />
+Added OpenWeather API.<br />
+User enters their mood. Bot asks for location. Based on both, it suggests a song. Everything's rule based right now.<br />
 
 Clone, create virtual environment, install packages.<br />
 Twilio account required.<br />
--- bot.py
+++ bot.py
@@ -1,19 +1,43 @@
 from flask import Flask, request
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
+import spotipy
+import sys
+from spotipy.oauth2 import SpotifyClientCredentials
+import json
+import random
 
 app = Flask(__name__)
 @app.route('/mm', methods = ['POST'])
 
 def bot():
-
     def get_mood(inc_msg):
         moods = ['good', 'happy', 'sad', 'sed', 'cool', 'emotional', 'angry', 'great', 'ecstatic']
         for word in moods:
             if word in inc_msg:
                 return word
-        return
+        return 'greet'
+
+    def get_song(curr_mood):
+        CLIENT_ID = ''
+        CLIENT_SECRET = ''
+        spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials(CLIENT_ID, CLIENT_SECRET))
+        results = spotify.search(q=curr_mood, type='track', limit=10)
+        rand_index = random.randint(0, 9)
+        return results["tracks"]["items"][rand_index]["external_urls"]["spotify"]
     
+    def get_weather(city):
+        base_url = 'https://api.openweathermap.org/data/2.5/weather'
+        API_KEY = ''
+        moods = ['good', 'happy', 'sad', 'sed', 'cool', 'emotional', 'angry', 'great', 'ecstatic']
+
+        if city.lower() != 'hi' and city.lower() != 'hello' and city.lower() not in moods :
+            weather = json.loads(requests.get(base_url, params = {'q' : city, 'appid' : API_KEY}).content)
+            weather_desc = weather['weather'][0]['description']
+            cloud_perc = weather['clouds']['all']
+            return (weather_desc, cloud_perc, city)
+        else:
+            return 'no_weather_data'
     
 
     incoming_msg = request.values.get('Body', '').lower()
@@ -21,18 +45,27 @@ def bot():
     msg = res.message()
     responded = False
     curr_mood = get_mood(incoming_msg)
+    moods = ['good', 'happy', 'sad', 'sed', 'cool', 'emotional', 'angry', 'great', 'ecstatic']
+    curr_weather = get_weather(incoming_msg)
+    ops = [curr_mood, curr_weather]
+    ran = random.randint(0, 1)
+    query = ops[ran]
+
     print(curr_mood)
-    if(curr_mood):
-        msg.body(f'''So, you're feeling {curr_mood} right now?\n
-                Let me suggest this song: \n''')
-    else:
+    if(curr_mood != 'greet'):
+        msg.body(f'''So, you're feeling {curr_mood} right now?\n''')
+        msg.body(f'''And where are you located right now🛐''')
+        responded = True
+    elif(curr_mood == 'greet' and curr_weather == 'no_weather_data'):
         if 'hi' in incoming_msg or 'hello' in incoming_msg:
             msg.body('Hello to you, too!\nHow are you feeling right now?\n')
             responded = True
         else:
             msg.body("Sorry! I am just a rule-based bot right now. I can't understand that.😢")
             responded = True
-    
+    else:
+        msg.body(f'''Alright. It feels like {curr_weather[0]} in {curr_weather[2]}.\nLet me suggest you this beautiful song:{get_song(query)}''')
+        responded = True
     return str(res)
 
 if __name__ == '__main__':
